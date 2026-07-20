8f2ecfe608f468081f3b9a33843d2f34969921b2
Vince Pileggi <vpileggi10@gmail.com>
2026-07-19 22:24:36 -0400

    Add license page, issue template, footer updates, and website refinements

--- docs/open-external-links.js
+++ docs/open-external-links.js
@@ -1,8 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const links = document.querySelectorAll('a[href^="http"]');
+  const links = document.querySelectorAll("a[href]");
 
   links.forEach(function (link) {
-    if (!link.href.includes(window.location.hostname)) {
+    const href = link.getAttribute("href");
+
+    if (
+      href &&
+      (href.startsWith("http://") || href.startsWith("https://")) &&
+      !href.includes(window.location.hostname)
+    ) {
       link.setAttribute("target", "_blank");
       link.setAttribute("rel", "noopener noreferrer");
     }
